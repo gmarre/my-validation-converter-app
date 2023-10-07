22092fd3968cd83206ad9396943ad4d286a57879
gmarre <gauthier.marre40@gmail.com>
2023-10-07 21:22:18 +0200

    Première tentative app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+// App.js
+import React, { useState } from 'react';
+import ScenarioForm from './ScenarioForm';
 import './App.css';
 
 function App() {
+  const [scenarios, setScenarios] = useState([]);
+
+  const handleAddScenario = (scenario) => {
+    setScenarios([...scenarios, scenario]);
+  };
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Application de Scénarios</h1>
+      <ScenarioForm onAddScenario={handleAddScenario} />
+      {/* Afficher les scénarios */}
+      <div>
+        <h2>Scénarios</h2>
+        <ul>
+          {scenarios.map((scenario, index) => (
+            <li key={index}>{scenario}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
